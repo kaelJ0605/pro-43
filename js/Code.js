206c3d8e7a60c06dce7a406c19ce1f5ad3c64c00
kaelJ0605 <89666409+kaelJ0605@users.noreply.github.com>
2022-04-19 18:38:51 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "13";
+const accessCode2 = "TODOS";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: qual o dia que eu nasci", 100, 70);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: qual meu chocolate favorito", 700, 170);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: guarda uma informação", 100, 270);
+
+}
